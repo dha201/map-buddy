9e8a3b01fe7049764419d7b2a567dbd73084c2d5
dha201 <davidmha2001@gmail.com>
2024-05-24 18:44:11 -0400

    Added Cards Components & Updated Banner

--- Components/Banner_Client.tsx
+++ Components/Banner_Client.tsx
@@ -1,6 +1,6 @@
-// Shortcut command: rfc -> Tab
 'use client';
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 export default function Banner_Client() {
 
@@ -9,12 +9,14 @@ export default function Banner_Client() {
     };
 
     return (
-        <div className="mb-12 grid lg:mb-24 lg:grid-cols-2">
+        <div className="relative mb-12 lg:mb-24 lg:grid lg:grid-cols-2 lg:gap-12">
           <div className="z-20 flex flex-col px-2 md:pt-12">
+
             <h1 className="my-2 flex-wrap">
               <span className="pr-2 text-white">Date</span>
               <span className="text-gray-900">Buddy</span>
             </h1>
+
             <p className="font-sans text-xl font-semibold text-gray-900 md:mt-5 lg:text-2xl">
                 Help you find the perfect Date!
             </p>
@@ -22,8 +24,18 @@ export default function Banner_Client() {
             <div className="mt-12">
               <button onClick={handleOnClick}>Find Dates</button>
             </div>
+
           </div>
+          <div className="relative z-10 lg:absolute lg:right-0 lg:top-10 lg:w-[50%]">
+              <Image
+                src="/static/hero-image.png"
+                width={800}
+                height={300}
+                alt="hero image"
+                priority={true}
+                className="relative"
+              />
+            </div>
         </div>
     );
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,62 @@
+/**
+ * Root page of the app
+ */
+
+
 import Image from "next/image";
 import Banner_Client from "@/Components/Banner_Client";
+import Card from "@/Components/Card_server";
 
 export default function Home() {
+  const datesID = [
+    {
+     "name": "Coffee Date",
+     "imageURL": "https://images.unsplash.com/photo-1548051072-b34898021f8b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+ 
+    {
+     "name": "Roof Top Date",
+     "imageURL": "https://images.unsplash.com/photo-1513279922550-250c2129b13a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }, 
+ 
+    {
+     "name": "Wine Date",
+     "imageURL": "https://images.unsplash.com/photo-1569929233287-f0565228c4d4?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+ 
+    {
+     "name": "Horse Riding Date",
+     "imageURL": "https://images.unsplash.com/photo-1559930477-620979bb1a39?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+ 
+    {
+     "name": "Explore Date",
+     "imageURL": "https://images.unsplash.com/photo-1466979939565-131c4b39a51b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+ 
+    {
+     "name": "Carnival Date",
+     "imageURL": "https://images.unsplash.com/photo-1569813363237-bd17086c234c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+ 
+    {
+     "name": "Boat Date",
+     "imageURL": "https://images.unsplash.com/photo-1558452919-3a47422e2fd0?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    } 
+  ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-14">
       <Banner_Client />
+
+      {datesID.map((date, idx) => (
+        <Card 
+          key={`${date}-{idx}`}
+          name={date.name} 
+          imageURL={date.imageURL}
+          href={`/dates/${date.name}`}
+        />
+      ))};
+
       <h3 className='font-interrrr'>Made with Love :)</h3>
     </main>
   );
